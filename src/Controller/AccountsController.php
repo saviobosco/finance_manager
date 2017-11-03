d8a56ef3c114166f2d1bbd2b70168d775238fc33
saviobosco <Johnboscoomebe@yahoo.com>
2017-11-03 20:43:14 +0100

    Update the add action in AccountsController.php

--- src/Controller/AccountsController.php
+++ src/Controller/AccountsController.php
@@ -8,6 +8,8 @@
 
 namespace App\Controller;
 
+use Cake\Utility\Inflector;
+use Cake\Utility\Text;
 use CakeDC\Users\Controller\Traits\LoginTrait;
 use CakeDC\Users\Controller\Traits\ProfileTrait;
 use CakeDC\Users\Controller\Traits\ReCaptchaTrait;
@@ -30,6 +32,28 @@ class AccountsController extends AppController
     public function dashboard()
     {
 
+    }
+
+    public function add()
+    {
+        $table = $this->loadModel();
+        $tableAlias = $table->alias();
+        $entity = $table->newEntity();
+        $this->set($tableAlias, $entity);
+        $this->set('tableAlias', $tableAlias);
+        $this->set('_serialize', [$tableAlias, 'tableAlias']);
+        if (!$this->request->is('post')) {
+            return;
+        }
+        $entity = $table->patchEntity($entity, $this->request->getData());
+        $entity->id = Text::uuid();
+        $singular = Inflector::singularize(Inflector::humanize($tableAlias));
+        if ($table->save($entity)) {
+            $this->Flash->success(__d('CakeDC/Users', 'The {0} has been saved', $singular));
+
+            return $this->redirect(['action' => 'index']);
+        }
+        $this->Flash->error(__d('CakeDC/Users', 'The {0} could not be saved', $singular));
     }
 
 }
